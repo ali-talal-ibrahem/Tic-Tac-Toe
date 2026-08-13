9b471d93be9ce65d2430313218469a0e4a82f871
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-13 23:44:50 +0300

    Remove Form1_Load event handler from Form1.Designer.cs
    The `this.Load` event handler, which was previously set to
    `this.Form1_Load`, has been removed from the `Form1.Designer.cs`
    file. This change prevents the `Form1_Load` method from being
    automatically invoked when the form is loaded.

--- Tic-Tac-Toe-Game/Form1.Designer.cs
+++ Tic-Tac-Toe-Game/Form1.Designer.cs
@@ -38,7 +38,6 @@
             this.ClientSize = new System.Drawing.Size(853, 484);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
