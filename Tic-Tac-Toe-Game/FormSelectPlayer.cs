0e188086754b78f52fdfbba249bf289e7f42c869
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-18 04:46:04 +0300

    Add FormSelectPlayer for player selection in Tic Tac Toe
    Introduced FormSelectPlayer class with constructor and event handlers for player selection buttons. Handlers instantiate Form1 with the selected player, display it as a dialog, and close the selection form.

---
+++ Tic-Tac-Toe-Game/FormSelectPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_Game
+{
+    public partial class FormSelectPlayer : Form
+    {
+        public FormSelectPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            short playerTag = Convert.ToInt16(button1.Tag);
+
+            this.Hide();
+
+            using (Form1 frm = new Form1(playerTag))
+            {
+                frm.ShowDialog();
+            }
+
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            short playerTag = Convert.ToInt16(button2.Tag);
+
+            this.Hide();
+
+            using (Form1 frm = new Form1(playerTag))
+            {
+                frm.ShowDialog();
+            }
+
+            this.Close();
+        }
+    }
+}
