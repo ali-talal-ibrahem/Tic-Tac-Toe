dcc8df4eb765a3f0486150eefe8f7653d79d9250
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-18 04:13:56 +0300

    Add winner check and improve endgame visuals
    Introduce CheckValues and CheckWinner methods to detect win conditions and highlight winning cards. Update EndGame to visually indicate the winner or a draw, including label and background changes. ChangeImage now checks for a winner after each move and handles draws appropriately. Replace all CARD_PLAYER1.Image and CARD_PLAYER2.Image references with BackgroundImage for consistency. Reset logic now also hides winner labels.

--- Tic-Tac-Toe-Game/Form1.cs
+++ Tic-Tac-Toe-Game/Form1.cs
@@ -39,18 +39,126 @@ namespace Tic_Tac_Toe_Game
         }
 
 
+
+        public bool CheckValues(Button btn1, Button btn2, Button btn3)
+        {
+
+            if (btn1.Tag.ToString() != "?" && btn1.Tag.ToString() == btn2.Tag.ToString() && btn1.Tag.ToString() == btn3.Tag.ToString())
+            {
+
+                btn1.BackColor = Color.Green;
+                btn2.BackColor = Color.Green;
+                btn3.BackColor = Color.Green;
+
+                if (btn1.Tag.ToString() == "X")
+                {
+
+                    GameStatus.Winner = enWinner.player1;
+                    GameStatus.GameOver = true;
+                    EndGame();
+                    return true;
+
+                }
+                else
+                {
+
+                    GameStatus.Winner = enWinner.player2;
+                    GameStatus.GameOver = true;
+                    EndGame();
+                    return true;
+
+                }
+
+            }
+
+            GameStatus.GameOver = false;
+            return false;
+
+        }
+
+
+        public void CheckWinner()
+        {
+
+            if (CheckValues(button1, button2, button3))
+                return;
+
+            if (CheckValues(button4, button5, button6))
+                return;
+
+            if (CheckValues(button7, button8, button9))
+                return;
+
+            if (CheckValues(button1, button4, button7))
+                return;
+
+            if (CheckValues(button2, button5, button8))
+                return;
+
+            if (CheckValues(button3, button6, button9))
+                return;
+
+            if (CheckValues(button1, button5, button9))
+                return;
+
+            if (CheckValues(button3, button5, button7))
+                return;
+
+        }
+
+
         public void EndGame()
         {
             if (GameStatus.Winner == enWinner.player1)
             {
+                CARD_PLAYER1.BackgroundImage = Properties.Resources.CARD_PLYAER_WINNER;
+                CARD_PLAYER2.BackgroundImage = Properties.Resources.CARD_PLYAER;
+
+                lb_Winner_Pl1.Text = "WINNER";
+                lb_Winner_Pl1.ForeColor = Color.Lime;
+                lb_Winner_Pl1.Visible = true;
+
+                lb_Winner_Pl2.Text = "Loser";
+                lb_Winner_Pl2.ForeColor = Color.Red;
+                lb_Winner_Pl2.Visible = true;
+
                 MessageBox.Show("Player 1 Wins", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else if (GameStatus.Winner == enWinner.player2)
             {
+                CARD_PLAYER2.BackgroundImage = Properties.Resources.CARD_PLYAER_WINNER;
+                CARD_PLAYER1.BackgroundImage = Properties.Resources.CARD_PLYAER;
+
+                Player1_lbl.ForeColor = Color.Yellow;
+                Player2_lbl.ForeColor = Color.Gray;
+
+                lb_Winner_Pl2.Text = "WINNER";
+                lb_Winner_Pl2.ForeColor = Color.Lime;
+                lb_Winner_Pl2.Visible = true;
+
+                lb_Winner_Pl1.Text = "Loser";
+                lb_Winner_Pl1.ForeColor = Color.Red;
+                lb_Winner_Pl1.Visible = true;
+
                 MessageBox.Show("Player 2 Wins", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else if (GameStatus.Winner == enWinner.Draw)
             {
+                CARD_PLAYER1.BackgroundImage = Properties.Resources.CARD_PLYAER_WINNER;
+                CARD_PLAYER2.BackgroundImage = Properties.Resources.CARD_PLYAER_WINNER;
+
+                Player2_lbl.ForeColor = Color.Yellow;
+                Player1_lbl.ForeColor = Color.Yellow;
+
+                lb_Winner_Pl1.Text = "Draw";
+                lb_Winner_Pl2.Text = "Draw";
+
+                lb_Winner_Pl1.ForeColor = Color.Yellow;
+                lb_Winner_Pl2.ForeColor = Color.Yellow;
+
+                lb_Winner_Pl1.Visible = true;
+                lb_Winner_Pl2.Visible = true;
+
                 MessageBox.Show("Draw", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
@@ -58,6 +166,7 @@ namespace Tic_Tac_Toe_Game
         public void ChangeImage(Button Btn) 
         {
 
+            if (GameStatus.GameOver) return;
 
             if (Btn.Tag.ToString() == "?") 
             { 
@@ -68,8 +177,8 @@ namespace Tic_Tac_Toe_Game
                     Player2_lbl.ForeColor = Color.Yellow;
                     Player1_lbl.ForeColor = Color.Gray;
 
-                    CARD_PLAYER1.Image = Properties.Resources.CARD_PLYAER;
-                    CARD_PLAYER2.Image = Properties.Resources.CARD_PLYAER_TURN;
+                    CARD_PLAYER1.BackgroundImage = Properties.Resources.CARD_PLYAER;
+                    CARD_PLAYER2.BackgroundImage = Properties.Resources.CARD_PLYAER_TURN;
                     CARD_PLAYER2.BackgroundImageLayout = ImageLayout.Stretch;
                     CARD_PLAYER1.BackgroundImageLayout = ImageLayout.Stretch;
 
@@ -78,6 +187,7 @@ namespace Tic_Tac_Toe_Game
 
                     PlayerTurn = enPlayerTurn.Player2;
                     GameStatus.PlayCount++;
+                    CheckWinner();
 
                     break;
 
@@ -86,8 +196,8 @@ namespace Tic_Tac_Toe_Game
                     Player1_lbl.ForeColor = Color.Yellow;
                     Player2_lbl.ForeColor = Color.Gray;
 
-                    CARD_PLAYER1.Image = Properties.Resources.CARD_PLYAER_TURN;
-                    CARD_PLAYER2.Image = Properties.Resources.CARD_PLYAER;
+                    CARD_PLAYER1.BackgroundImage = Properties.Resources.CARD_PLYAER_TURN;
+                    CARD_PLAYER2.BackgroundImage = Properties.Resources.CARD_PLYAER;
                     CARD_PLAYER2.BackgroundImageLayout = ImageLayout.Stretch;
                     CARD_PLAYER1.BackgroundImageLayout = ImageLayout.Stretch;
 
@@ -96,17 +206,17 @@ namespace Tic_Tac_Toe_Game
 
                     PlayerTurn = enPlayerTurn.Player1;
                     GameStatus.PlayCount++;
-
+                    CheckWinner();
+                    
                     break;
 
             }
 
-                if (GameStatus.PlayCount == 9)
+                if (GameStatus.PlayCount == 9 && !GameStatus.GameOver)
                 {
                     GameStatus.GameOver = true;
                     GameStatus.Winner = enWinner.Draw;
                     EndGame();
-
                 }
             }
 
@@ -148,8 +258,11 @@ namespace Tic_Tac_Toe_Game
             PlayerTurn = enPlayerTurn.Player1;
             Player1_lbl.ForeColor = Color.Yellow;
             Player2_lbl.ForeColor = Color.Gray;
-            CARD_PLAYER1.Image = Properties.Resources.CARD_PLYAER_TURN;
-            CARD_PLAYER2.Image = Properties.Resources.CARD_PLYAER;
+            CARD_PLAYER1.BackgroundImage = Properties.Resources.CARD_PLYAER_TURN;
+            CARD_PLAYER2.BackgroundImage = Properties.Resources.CARD_PLYAER;
+            lb_Winner_Pl1.Visible = false;
+            lb_Winner_Pl2.Visible = false;
+
             GameStatus.PlayCount = 0;
             GameStatus.GameOver = false;
             GameStatus.Winner = enWinner.GameInProgress;
@@ -163,4 +276,4 @@ namespace Tic_Tac_Toe_Game
         }
 
     }
-}
+}   
