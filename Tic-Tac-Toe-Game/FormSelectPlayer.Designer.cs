bcc1f52c945e5954806e3b9b042bfc733cf4d606
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-18 04:46:15 +0300

    Add FormSelectPlayer partial class with player selection UI
    Added a new FormSelectPlayer partial class in the Tic_Tac_Toe_Game namespace. The form includes a label prompting player selection, two image-styled buttons for "X" and "O" with click event handlers, and a custom background image. Configured resource references and layout properties for all controls.

---
+++ Tic-Tac-Toe-Game/FormSelectPlayer.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Tic_Tac_Toe_Game
+{
+    partial class FormSelectPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Alexandria SemiBold", 30F);
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(113, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(601, 70);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "What Do You Want To Play ?";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::Tic_Tac_Toe_Game.Properties.Resources.X;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(147, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 175);
+            this.button1.TabIndex = 17;
+            this.button1.Tag = "0";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::Tic_Tac_Toe_Game.Properties.Resources.O;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(485, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 175);
+            this.button2.TabIndex = 18;
+            this.button2.Tag = "1";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FormSelectPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Tic_Tac_Toe_Game.Properties.Resources.BACKGROUND;
+            this.ClientSize = new System.Drawing.Size(827, 496);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormSelectPlayer";
+            this.Text = "FormSelectPlayer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
