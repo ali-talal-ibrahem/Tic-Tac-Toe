543e24a65b46b0f013860fc35e49404298087e67
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-18 04:54:39 +0300

    Update form resources and set title to "Tic Tac Toe"
    Added ComponentResourceManager for resource handling.
    Set form Icon property from resources.
    Renamed form title from "FormSelectPlayer" to "Tic Tac Toe".

--- Tic-Tac-Toe-Game/FormSelectPlayer.Designer.cs
+++ Tic-Tac-Toe-Game/FormSelectPlayer.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSelectPlayer));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -90,8 +91,9 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormSelectPlayer";
-            this.Text = "FormSelectPlayer";
+            this.Text = "Tic Tac Toe";
             this.ResumeLayout(false);
             this.PerformLayout();
 
