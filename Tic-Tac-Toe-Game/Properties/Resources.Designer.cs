bae5fc2a6619bdabade18c449ef2c9b7a31c2344
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-08-18 04:13:25 +0300

    Update resources: add CARD_PLYAER_WINNER, reorder entries
    Resources.Designer.cs updated for StronglyTypedResourceBuilder 18.0.0.0. Added CARD_PLYAER_WINNER property and bitmap resource. Reordered and regrouped Resources.resx entries; no resources removed.

--- Tic-Tac-Toe-Game/Properties/Resources.Designer.cs
+++ Tic-Tac-Toe-Game/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Tic_Tac_Toe_Game.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -96,6 +96,16 @@ namespace Tic_Tac_Toe_Game.Properties {
         internal static System.Drawing.Bitmap CARD_PLYAER_TURN {
             get {
                 object obj = ResourceManager.GetObject("CARD_PLYAER_TURN", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap CARD_PLYAER_WINNER {
+            get {
+                object obj = ResourceManager.GetObject("CARD_PLYAER_WINNER", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
